eee17b5c8b1132db40a57df0f954783fc4121956
Raj Patel <126887227+rajpat739407@users.noreply.github.com>
2025-10-05 04:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import pickle
 from scipy.sparse import load_npz
-# import plotly.express as px
-# import plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
 import os
 import sys
 
